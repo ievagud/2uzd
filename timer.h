7bf69559334878f1c6367b408c8bcaa3b1cdbdd7
ievagud <144427953+ievagud@users.noreply.github.com>
2023-11-25 17:11:05 +0200

    Add files via upload

---
+++ timer.h
@@ -0,0 +1,45 @@
+/*#ifndef TIMER_H_INCLUDED
+#define TIMER_H_INCLUDED
+
+#include <chrono>
+
+class Timer {
+    private:
+        std::chrono::time_point<std::chrono::high_resolution_clock> start;
+    public:
+        Timer() : start{std::chrono::high_resolution_clock::now()} {}
+        void reset() {
+            start = std::chrono::high_resolution_clock::now();
+        }
+        double elapsed() const {
+            return std::chrono::duration<double>
+            (std::chrono::high_resolution_clock::now() - start).count();
+        }
+};*/
+
+#ifndef TIMER_H_INCLUDED
+#define TIMER_H_INCLUDED
+
+#include <chrono>
+
+class Timer {
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+
+public:
+    Timer() {
+        start = std::chrono::high_resolution_clock::now();
+    }
+
+    void reset() {
+        start = std::chrono::high_resolution_clock::now();
+    }
+
+    double elapsed() const {
+        auto now = std::chrono::high_resolution_clock::now();
+        return std::chrono::duration<double>(now - start).count();
+    }
+};
+
+#endif // TIMER_H_INCLUDED
+
