eb2b123caca5b7a1210fcb310863d9b634ac9d5a
ievagud <144427953+ievagud@users.noreply.github.com>
2023-12-28 18:24:16 +0200

    Add files via upload

---
+++ html/class_zmogus.js
@@ -0,0 +1,9 @@
+var class_zmogus =
+[
+    [ "Zmogus", "class_zmogus.html#aa7a8ba4d3c4778f9b35d59eef3e72574", null ],
+    [ "Zmogus", "class_zmogus.html#a332158708aad76d01cef6d07e0919974", null ],
+    [ "~Zmogus", "class_zmogus.html#ac5615bf607a8f2f1b303ffa04328d24d", null ],
+    [ "kalba", "class_zmogus.html#aee44ab07c62889fa6fe221567fdbeafc", null ],
+    [ "pavarde", "class_zmogus.html#a00799e1396c82e4f6dffa0edec1f24e5", null ],
+    [ "vardas", "class_zmogus.html#a56da52b45c537a31d2c1fc3a53a73c65", null ]
+];
